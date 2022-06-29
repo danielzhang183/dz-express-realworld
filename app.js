636a15d4a04ad44d07ce12a35a96c0fabe2315a3
danielzhang183 <danielzhang182@gmail.com>
2022-06-30 02:23:22 +0800

    base: add cros middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 // add logger
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(cors())
 const PORT = process.env.port || 3000
 
 app.get('/', (req, res) => {
