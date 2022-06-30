5c26e31a15e35f1926f1e2e4897431e91d7a815f
danielzhang183 <danielzhang182@gmail.com>
2022-06-30 12:48:31 +0800

    chore: add express dependency & basic usage

--- router/user.js
+++ router/user.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const router = express.Router()
 const userCtrl = require('../controller/user')
+const { body, validationResult } = require('express-validator')
+const { User } = require('../model')
 
 // 登录
 router.post('/users/login', userCtrl.login)
 
 // 注册
-router.post('/users', userCtrl.register)
+router.post('/users', [
+  body('user.username')
+    .notEmpty().withMessage('用户名不能为空')
+    .custom(async (username) => {
+      const user = await User.findOne({ username })
+      if (user) {
+        return Promise.reject('用户名已存在')
+      }
+    }),
+  body('user.password').notEmpty().withMessage('密码不能为空'),
+  body('user.email')
+    .notEmpty().withMessage('邮箱不能为空')
+    .isEmail().withMessage('邮箱格式不正确')
+    .bail()
+    .custom(async (email) => {
+      const user = await User.findOne({ email })
+      if (user) {
+        return Promise.reject('邮箱已存在')
+      }
+    })
+], (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  next()
+}, userCtrl.register)
 
 // 获取当前登录用户
 router.get('/user', userCtrl.getCurrentUser)
