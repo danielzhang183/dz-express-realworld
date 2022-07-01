464088d1f5449fa286f7f508d9fa3698c9ba11a9
danielzhang183 <danielzhang182@gmail.com>
2022-07-01 10:53:07 +0800

    chore: add jwt logic & generate token

--- config/config.default.js
+++ config/config.default.js
@@ -1,3 +1,4 @@
 module.exports = {
-  dbUri: 'mongodb://admin:Abc123__@192.168.22.130:27017/admin'
+  dbUri: 'mongodb://admin:Abc123__@192.168.22.130:27017/admin',
+  jwtSecret: 'd6a0e3b0-1d79-467d-9016-179940e2691d '
 }
--- controller/user.js
+++ controller/user.js
@@ -1,8 +1,12 @@
 const { User } = require('../model')
+const jwt = require('../util/jwt')
+const { jwtSecret } = require('../config/config.default')
 
 exports.login = async (req, res, next) => {
   try {
-    res.send('post /users/login')
+    const user = req.user.toJSON()
+    const token = await jwt.sign({ userId: user._id }, jwtSecret)
+    res.status(200).json({ ...user, token })
   } catch (err) {
     next(err)
   }
